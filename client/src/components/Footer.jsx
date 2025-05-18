5d168d5e4a9a9bdbb612d050daa78b1d85236bd8
SHIV <72968797+imdevshiv@users.noreply.github.com>
2025-05-19 04:27:11 +0530

    update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-const Footer = () => (
-  <footer className="h-16 flex items-center justify-center text-gray-500 border-t mt-10 ">
-    © 2025 Omnify. All rights reserved
-  </footer>
-);
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="h-16 flex items-center justify-center text-gray-500 border-t mt-10 text-center px-2">
+      All rights reserved © {year} Omnify. | Made with ❤️ by{" "}
+      <a
+        href="https://github.com/imdevshiv"
+        className="text-blue-500 hover:underline ml-1"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Shiv
+      </a>
+    </footer>
+  );
+};
 
 export default Footer;
